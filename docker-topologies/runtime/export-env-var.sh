53eb6c125ed3aa779afeb4a9506591b7171422b5
Jonathan Dreyer <jonathan.dreyer@cleanenergie.ch>
2017-05-19 13:12:05 +0200

    Improve script of env. var

--- docker-topologies/runtime/export-env-var.sh
+++ docker-topologies/runtime/export-env-var.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+if [ -z ${AA_TOKEN+x} ]; then echo "Token is unset"; exit 1; else echo "Token is set to '$AA_TOKEN'"; fi
+if [ -z ${AA_REPOS+x} ]; then echo "Repos is unset"; exit 1; else echo "Repos is set to '$AA_REPOS'"; fi
+
 echo TOKEN=$AA_TOKEN > .env
 echo REPOS=$AA_REPOS >> .env
-echo DELAY=$AA_DELAY >> .env
-
-echo
-echo 'result file .env :'
-more .env
+if ! [ -z ${AA_DELAY+x} ]; then echo DELAY=$AA_DELAY >> .env; fi
